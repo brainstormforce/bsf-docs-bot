727a84fd7bf12531ed1ea339a92ac534b3279914
Navanath Bhosale <navanathb@bsf.io>
2023-10-27 10:53:50 +0530

    refactor: v1.0.1 release

--- bsf-docs-bot.php
+++ bsf-docs-bot.php
@@ -4,7 +4,7 @@
  * Description: This ultimate tool for managing and presenting interactive documentation with ease and efficiency.
  * Author: Brainstorm Force, Navanath Bhosale
  * Author URI: https://github.com/brainstormforce/bsf-docs-bot
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL v2
  * Text Domain: bsf-docs-bot
  *
@@ -18,7 +18,7 @@ define( 'BSF_DOCS_BOT_FILE', __FILE__ );
 define( 'BSF_DOCS_BOT_BASE', plugin_basename( BSF_DOCS_BOT_FILE ) );
 define( 'BSF_DOCS_BOT_DIR', plugin_dir_path( BSF_DOCS_BOT_FILE ) );
 define( 'BSF_DOCS_BOT_URL', plugins_url( '/', BSF_DOCS_BOT_FILE ) );
-define( 'BSF_DOCS_BOT_VER', '1.0.0' );
+define( 'BSF_DOCS_BOT_VER', '1.0.1' );
 define( 'BSF_DOCS_BOT_ADMIN_SLUG', 'bsf_docs_bot' );
 define( 'BSF_DOCS_BOT_DB_OPTION', 'bsf_docs_bot_settings' );
 
